cd0fede58535831057d0d0af025588ea88b742d7
Eric <ericscottlagergren@gmail.com>
2014-05-20 01:12:15 -0700

    updated css specifically footer and byline and edited mobile nav

--- js/master.js
+++ js/master.js
@@ -1,79 +1,78 @@
 (function() {
-    var ab = window;
-
-    function aa(e) {
-        var d = Z,
-            h;
-        e: {
-            h = d.className.split(/\s+/);
-            for (var c = 0, f = h.length; c < f; c += 1) {
-                if (h[c] === e) {
-                    h = !0;
-                    break e
+    function X(d) {
+        var b = z,
+            f;
+        d: {
+            f = b.className.split(/\s+/);
+            for (var c = 0, a = f.length; c < a; c += 1) {
+                if (f[c] === d) {
+                    f = !0;
+                    break d;
                 }
             }
-            h = !1
+            f = !1;
         }
-        h || (d.className += ("" === d.className ? "" : " ") + e)
+        f || (b.className += ("" === b.className ? "" : " ") + d);
     }
 
-    function Y(d, c, e) {
-        this.b = null != d ? d : null;
-        this.c = null != c ? c : null;
-        this.e = null != e ? e : null
-    }
-    var X = /^([0-9]+)(?:[\._-]([0-9]+))?(?:[\._-]([0-9]+))?(?:[\._+-]?(.*))?$/;
-
-    function W(d, c) {
-        return d.b > c.b || d.b === c.b && d.c > c.c || d.b === c.b && d.c === c.c && d.e > c.e ? 1 : d.b < c.b || d.b === c.b && d.c < c.c || d.b === c.b && d.c === c.c && d.e < c.e ? -1 : 0
+    function H(b, a, c) {
+        this.b = null != b ? b : null;
+        this.c = null != a ? a : null;
+        this.e = null != c ? c : null;
     }
 
-    function V(d, c) {
-        return 0 === W(d, c) || 1 === W(d, c)
+    function O(b, a) {
+        return b.b > a.b || b.b === a.b && b.c > a.c || b.b === a.b && b.c === a.c && b.e > a.e ? 1 : b.b < a.b || b.b === a.b && b.c < a.c || b.b === a.b && b.c === a.c && b.e < a.e ? -1 : 0;
     }
 
-    function U() {
-        var e = X.exec(T[1]),
-            d = null,
-            f = null,
-            c = null;
-        e && (null !== e[1] && e[1] && (d = parseInt(e[1], 10)), null !== e[2] && e[2] && (f = parseInt(e[2], 10)), null !== e[3] && e[3] && (c = parseInt(e[3], 10)));
-        return new Y(d, f, c)
+    function K(b, a) {
+        return 0 === O(b, a) || 1 === O(b, a);
     }
 
     function R() {
-        var c = P;
-        return 3 === c.a || 7 === c.a || 6 === c.a || 9 === c.a || 8 === c.a || 5 === c.a ? "grayscale" : 1 === c.a && V(c.f, new Y(6, 2)) && 1 === c.d ? "grayscale" : "unknown"
+        var c = F.exec(Y[1]),
+            d = null,
+            b = null,
+            a = null;
+        c && (null !== c[1] && c[1] && (d = parseInt(c[1], 10)), null !== c[2] && c[2] && (b = parseInt(c[2], 10)), null !== c[3] && c[3] && (a = parseInt(c[3], 10)));
+        return new H(d, b, a);
     }
-    var P, N = ab.navigator.userAgent,
-        L = 0,
-        J = new Y,
-        o = 0,
-        j = new Y,
-        T = null;
-    if (T = /(?:iPod|iPad|iPhone).*? OS ([\d_]+)/.exec(N)) {
-        o = 3, j = U()
+
+    function I() {
+        var a = ab;
+        return 3 === a.a || 7 === a.a || 6 === a.a || 9 === a.a || 8 === a.a || 5 === a.a ? "grayscale" : 1 === a.a && K(a.f, new H(6, 2)) && 1 === a.d ? "grayscale" : "unknown";
+    }
+    var M = window;
+    var F = /^([0-9]+)(?:[\._-]([0-9]+))?(?:[\._-]([0-9]+))?(?:[\._+-]?(.*))?$/;
+    var ab, W = M.navigator.userAgent,
+        Q = 0,
+        Z = new H,
+        U = 0,
+        L = new H,
+        Y = null;
+    if (Y = /(?:iPod|iPad|iPhone).*? OS ([\d_]+)/.exec(W)) {
+        U = 3, L = R();
     } else {
-        if (T = /(?:BB\d{2}|BlackBerry).*?Version\/([^\s]*)/.exec(N)) {
-            o = 9, j = U()
+        if (Y = /(?:BB\d{2}|BlackBerry).*?Version\/([^\s]*)/.exec(W)) {
+            U = 9, L = R();
         } else {
-            if (T = /Android ([^;)]+)|Android/.exec(N)) {
-                o = 5, j = U()
+            if (Y = /Android ([^;)]+)|Android/.exec(W)) {
+                U = 5, L = R();
             } else {
-                if (T = /Windows Phone(?: OS)? ([^;)]+)/.exec(N)) {
-                    o = 8, j = U()
+                if (Y = /Windows Phone(?: OS)? ([^;)]+)/.exec(W)) {
+                    U = 8, L = R();
                 } else {
-                    if (T = /Linux ([^;)]+)|Linux/.exec(N)) {
-                        o = 4, j = U()
+                    if (Y = /Linux ([^;)]+)|Linux/.exec(W)) {
+                        U = 4, L = R();
                     } else {
-                        if (T = /OS X ([^;)]+)/.exec(N)) {
-                            o = 2, j = U()
+                        if (Y = /OS X ([^;)]+)/.exec(W)) {
+                            U = 2, L = R();
                         } else {
-                            if (T = /Windows NT ([^;)]+)/.exec(N)) {
-                                o = 1, j = U()
+                            if (Y = /Windows NT ([^;)]+)/.exec(W)) {
+                                U = 1, L = R();
                             } else {
-                                if (T = /CrOS ([^;)]+)/.exec(N)) {
-                                    o = 6, j = U()
+                                if (Y = /CrOS ([^;)]+)/.exec(W)) {
+                                    U = 6, L = R();
                                 }
                             }
                         }
@@ -81,35 +80,35 @@
                 }
             }
         }
-    } if (T = /MSIE ([\d\w\.]+)/.exec(N)) {
-        L = 1, J = U()
+    } if (Y = /MSIE ([\d\w\.]+)/.exec(W)) {
+        Q = 1, Z = R();
     } else {
-        if (T = /Trident.*rv:([\d\w\.]+)/.exec(N)) {
-            L = 1, J = U()
+        if (Y = /Trident.*rv:([\d\w\.]+)/.exec(W)) {
+            Q = 1, Z = R();
         } else {
-            if (T = /OPR\/([\d.]+)/.exec(N)) {
-                L = 4, J = U()
+            if (Y = /OPR\/([\d.]+)/.exec(W)) {
+                Q = 4, Z = R();
             } else {
-                if (T = /Opera Mini.*Version\/([\d\.]+)/.exec(N)) {
-                    L = 4, J = U()
+                if (Y = /Opera Mini.*Version\/([\d\.]+)/.exec(W)) {
+                    Q = 4, Z = R();
                 } else {
-                    if (T = /Opera(?: |.*Version\/|\/)([\d\.]+)/.exec(N)) {
-                        L = 4, J = U()
+                    if (Y = /Opera(?: |.*Version\/|\/)([\d\.]+)/.exec(W)) {
+                        Q = 4, Z = R();
                     } else {
-                        if (T = /Firefox\/([\d\w\.]+)|Firefox/.exec(N)) {
-                            L = 3, J = U()
+                        if (Y = /Firefox\/([\d\w\.]+)|Firefox/.exec(W)) {
+                            Q = 3, Z = R();
                         } else {
-                            if (T = /(?:Chrome|CrMo|CriOS)\/([\d\.]+)/.exec(N)) {
-                                L = 2, J = U()
+                            if (Y = /(?:Chrome|CrMo|CriOS)\/([\d\.]+)/.exec(W)) {
+                                Q = 2, Z = R();
                             } else {
-                                if (T = /Silk\/([\d\._]+)/.exec(N)) {
-                                    L = 7, J = U()
+                                if (Y = /Silk\/([\d\._]+)/.exec(W)) {
+                                    Q = 7, Z = R();
                                 } else {
-                                    if (5 === o || 9 === o) {
-                                        L = 6
+                                    if (5 === U || 9 === U) {
+                                        Q = 6;
                                     } else {
-                                        if (T = /Version\/([\d\.\w]+).*Safari/.exec(N)) {
-                                            L = 5, J = U()
+                                        if (Y = /Version\/([\d\.\w]+).*Safari/.exec(W)) {
+                                            Q = 5, Z = R();
                                         }
                                     }
                                 }
@@ -120,61 +119,49 @@
             }
         }
     }
-    P = new function(e, d, f, c) {
-        this.d = e;
-        this.g = d;
-        this.a = f;
-        this.f = c
-    }(L, J, o, j);
-    var i = R(),
-        b, a = R();
-    b = "unknown" !== a ? a : 2 === P.a || 4 === P.a ? "subpixel" : 1 === P.a ? V(P.f, new Y(6, 0)) ? "subpixel" : 1 === P.d ? V(P.g, new Y(7, 0)) ? "subpixel" : "grayscale" : "subpixel" : "unknown";
-    var Z = ab.document.documentElement,
-        S;
-    if (1 === P.a) {
-        var Q, O;
-        if (!(O = 2 === P.d)) {
-            var M;
-            (M = 4 === P.d) || (M = -1 === W(P.f, new Y(6, 0)));
-            O = M
+    ab = new function(c, a, d, b) {
+        this.d = c;
+        this.g = a;
+        this.a = d;
+        this.f = b;
+    }(Q, Z, U, L);
+    var D = I(),
+        P, V = I();
+    P = "unknown" !== V ? V : 2 === ab.a || 4 === ab.a ? "subpixel" : 1 === ab.a ? K(ab.f, new H(6, 0)) ? "subpixel" : 1 === ab.d ? K(ab.g, new H(7, 0)) ? "subpixel" : "grayscale" : "subpixel" : "unknown";
+    var z = M.document.documentElement,
+        aa;
+    if (1 === ab.a) {
+        var B, G;
+        if (!(G = 2 === ab.d)) {
+            var k;
+            (k = 4 === ab.d) || (k = -1 === O(ab.f, new H(6, 0)));
+            G = k;
         }
-        if (O) {
-            Q = "gdi"
+        if (G) {
+            B = "gdi";
         } else {
-            var K;
-            if (V(P.f, new Y(6, 0))) {
-                var H;
-                if (H = 1 === P.d) {
-                    var m = P.g,
-                        g = new Y(8, 0);
-                    H = 0 === W(m, g) || -1 === W(m, g)
+            var A;
+            if (K(ab.f, new H(6, 0))) {
+                var j;
+                if (j = 1 === ab.d) {
+                    var q = ab.g,
+                        J = new H(8, 0);
+                    j = 0 === O(q, J) || -1 === O(q, J);
                 }
-                K = H ? "gdi" : "directwrite"
+                A = j ? "gdi" : "directwrite";
             } else {
-                K = "unknown"
+                A = "unknown";
             }
-            Q = K
+            B = A;
         }
-        S = Q
+        aa = B;
     } else {
-        S = 8 === P.a ? "directwrite" : 2 === P.a || 3 === P.a ? "coretext" : 5 === P.a || 4 === P.a || 6 === P.a || 7 === P.a || 9 === P.a ? "freetype" : "unknown"
+        aa = 8 === ab.a ? "directwrite" : 2 === ab.a || 3 === ab.a ? "coretext" : 5 === ab.a || 4 === ab.a || 6 === ab.a || 7 === ab.a || 9 === ab.a ? "freetype" : "unknown";
     }
-    aa("tr-" + S);
-    "unknown" === i && "unknown" !== b && (i += "-" + b);
-    aa("tr-aa-" + i)
-}());
-(function(d, e, j, h, f, c, b) {
-    d.GoogleAnalyticsObject = f;
-    d[f] = d[f] || function() {
-        (d[f].q = d[f].q || []).push(arguments)
-    }, d[f].l = 1 * new Date();
-    c = e.createElement(j), b = e.getElementsByTagName(j)[0];
-    c.async = 1;
-    c.src = h;
-    b.parentNode.insertBefore(c, b)
-})(window, document, "script", "//www.google-analytics.com/analytics.js", "ga");
-ga("create", "UA-45524949-1", "ericlagergren.com");
-ga("send", "pageview");
+    X("tr-" + aa);
+    "unknown" === D && "unknown" !== P && (D += "-" + P);
+    X("tr-aa-" + D);
+})();
 $(document).ready(function() {
     $(".show_navigation").click(function() {
         $(".nav-main").css({
@@ -185,18 +172,20 @@ $(document).ready(function() {
             "border-bottom": "1px dotted #333"
         });
         if ($(window).width() <= 1000) {
-            $("body").css({"margin-top":"253px"})
+            $("body").css({
+                "margin-top": "255px"
+            });
         } else {
             $("body").css({
-            "margin-top": "199px"
-        })
-        };
+                "margin-top": "199px"
+            });
+        }
         $(".show_navigation").css({
             display: "none"
         });
         $(".close_navigation").css({
             display: "block"
-        })
+        });
     });
     $(".close_navigation").click(function() {
         $(".nav-main").css({
@@ -213,6 +202,44 @@ $(document).ready(function() {
         });
         $(".show_navigation").css({
             display: "block"
-        })
-    })
+        });
+    });
+});
+var disqus_shortname = "ericlagergren";
+(function() {
+    var a = document.createElement("script");
+    a.type = "text/javascript";
+    a.async = true;
+    a.src = "//" + disqus_shortname + ".disqus.com/embed.js";
+    (document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0]).appendChild(a);
+})();
+$(document).ready(function() {
+    $("#note").click(function(a) {
+        a.preventDefault();
+        $("#note-body").slideToggle("slow");
+    });
+});
+var hashTagActive = "";
+$(".scroll").click(function(b) {
+    if (hashTagActive != this.hash) {
+        b.preventDefault();
+        var a = 0;
+        if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
+            a = $(document).height() - $(window).height();
+        } else {
+            a = $(this.hash).offset().top;
+        }
+        $("html,body").animate({
+            scrollTop: a
+        }, 2000, "swing");
+        hashTagActive = this.hash;
+    }
+});
+$(".to-top").hide();
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 200) {
+        $(".to-top").fadeIn();
+    } else {
+        $(".to-top").fadeOut();
+    }
 });
